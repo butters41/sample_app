20b1b06a30460057dd669e6661371c5307d10c2e
Jason Butterfield <butters41@hotmail.com>
2014-05-17 00:01:52 -0400

    finish user edit, update, index, and destroy actions

--- Gemfile
+++ Gemfile
@@ -5,13 +5,18 @@ gem 'rails', '4.0.4'
 gem 'bootstrap-sass', '2.3.2.0'
 gem 'sprockets', '2.11.0'
 gem 'bcrypt-ruby', '3.1.2'
+gem 'faker', '1.1.2'
+gem 'pg'
+gem 'will_paginate', '3.0.4'
+gem 'bootstrap-will_paginate', '0.0.9'
 
 group :development, :test do
 
 	# Use oracle
-	gem 'ruby-oci8'
+	# gem 'ruby-oci8'
 
-	gem 'activerecord-oracle_enhanced-adapter', '>=1.5.0'
+	# gem 'activerecord-oracle_enhanced-adapter', '>=1.5.0'
+
 
 	gem 'rspec-rails'
 end
@@ -60,6 +65,5 @@ end
 # gem 'debugger', group: [:development, :test]
 
 group :production do
-	gem 'pg'
 	gem 'rails_12factor'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140505145000) do
+ActiveRecord::Schema.define(version: 20140517031207) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
 
   create_table "users", force: true do |t|
     t.string   "name"
@@ -20,9 +23,10 @@ ActiveRecord::Schema.define(version: 20140505145000) do
     t.datetime "updated_at"
     t.string   "password_digest"
     t.string   "remember_token"
+    t.boolean  "admin",           default: false
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["remember_token"], name: "index_users_on_remember_token"
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["remember_token"], name: "index_users_on_remember_token", using: :btree
 
 end
